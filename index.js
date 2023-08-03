be28dbe576428bd0878dbdae5a934e1f8fc8332f
Stefan Alexandru <stefanalecsandru@yahoo.com>
2023-08-04 00:34:23 +0300

    latest updates

--- index.js
+++ index.js
@@ -7,28 +7,40 @@ const answer = document.getElementById('answer');
 
 function bmi() { // functie care calculeaza BMI
 
-    const weightValue = weight.value;  // valoarea introdusa de user in campul de greutate
-    const heightValue = height.value / 100; // valoarea introdusa de user in campul de inaltime
+    const weightValue = parseFloat(weight.value);  // valoarea introdusa de user in campul de greutate
+    const heightValue = parseFloat(height.value) / 100; // valoarea introdusa de user in campul de inaltime
+
+
+    if (isNaN(weightValue) || isNaN(heightValue)) {
+        answer.innerText = ' Please enter valid data for Weight & Height.';
+        answer.style.color = 'red';
+        answer.style.fontWeight = 'bold';
+        return; 
+    }
 
     const bmiValue = weightValue / (heightValue * heightValue); // calcul BMI
 
     result.value = Math.round(bmiValue); // adauga valoarea BMI in campul final pentru vizualizare rezultat
-
+    
     if (bmiValue < 18.5) {
         answer.innerText = ' Danger of anorexia';
         answer.style.color = 'red';
+        answer.style.fontWeight = 'bold';
     }
     if (bmiValue < 25 && bmiValue >= 18) {
         answer.innerHTML = ' You are fine';
         answer.style.color = 'red';
+        answer.style.fontWeight = 'bold';
     }
     if (bmiValue < 30 && bmiValue >= 25) {
         answer.innerHTML = ' Try the gym';
         answer.style.color = 'red';
+        answer.style.fontWeight = 'bold';
     }
     if (bmiValue > 30) {
         answer.innerHTML = ' Quit mcdonalds';
         answer.style.color = 'red';
+        answer.style.fontWeight = 'bold';
     }
 
 }
